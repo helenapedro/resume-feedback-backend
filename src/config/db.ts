d5687e45d2f24e5e961a802fa10518bd7012815d
Helena Pedro <91161075+helenapedro@users.noreply.github.com>
2024-10-23 21:05:41 +0100

    Add files via upload
    new commit

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,7 +5,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI || '');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    if (error instanceof Error) {
+    if (error instanceof Error) { 
       console.error(`Error: ${error.message}`);
     } else {
       console.error('Unknown error during MongoDB connection');
